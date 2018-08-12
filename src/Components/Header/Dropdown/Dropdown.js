b960a69a1df9b15aa830408f40a2617891d9fd20
Ricardo Agra <agra.ric@gmail.com>
2018-08-12 19:31:41 +0100

    New header implemented

---
+++ src/Components/Header/Dropdown/Dropdown.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Styles from './Dropdown.css';
+
+const Dropdown = ( props ) => {
+    let links = [];
+    props.links.forEach( link => {
+        links.push(
+            <li>
+                <a className={ Styles[ link.class ] || "" } href={ link.href }>{ link.title }</a>
+            </li>
+        )
+    });
+
+    let dropdownClasses = [ Styles.dropdown ];
+    if( props.selected ){
+        dropdownClasses.push( Styles.visible );
+    }
+
+    return(
+        <div class={ dropdownClasses.join( " " ) } onClick={ props.click }>
+            <h2>{ props.title }<span className="fa fa-caret-down"></span></h2>
+            <ul class={ Styles.menu }>  
+                { links }
+            </ul>
+        </div>
+    );
+}
+
+export default Dropdown;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,131 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import Styles from './Header.css';
 
-const header = ( props ) => {
-    return(
-        <header id="header" className={ Styles.header }>
-            <img className={ Styles.logo } src={ require("../../Assets/logo_small.png") } alt="multi-colored petals" />
-            <h1 className={ Styles.title }>Travel Agency</h1>
-        </header>
-    );
+import Dropdown from './Dropdown/Dropdown'
+
+let destinations = [ "africa","europe","americas","asia","oceania" ];
+
+class Header extends Component {
+    state = {
+        isMenuVisible: false,
+        isDestinationVisible: false,
+        isAboutVisible: false,
+        isSearchError: false
+    }
+
+    componentDidMount() {
+        window.onclick = () => {
+            this.setState({
+                isDestinationVisible: false,
+                isAboutVisible: false
+            });
+        }
+    }
+
+    searchHandler = () => {
+        let input = document.getElementById( "search-input" ).value;
+        let newState = {
+            isSearchError: true
+        }
+
+        if( /^[a-zA-Z ]+$/.test( input ) ){
+            newState.isSearchError = destinations.indexOf( input.toLowerCase() ) === -1;
+        }
+    
+        this.setState( newState );
+
+        window.setTimeout( ()=>{ this.setState({ isSearchError: false }) },5000 );
+    }
+
+    enableMenuHandler = () => {
+        let newState = { 
+            isMenuVisible: !this.state.isMenuVisible,
+            isDestinationVisible: false,
+            isAboutVisible: false
+        }
+
+        this.setState( newState );
+    }
+
+    dropdownHandler = ( event, dropdown ) => {
+        event.stopPropagation();
+
+        let newState = {
+            isDestinationVisible: false,
+            isAboutVisible: false
+        };
+        
+        newState[ dropdown ] = !this.state[ dropdown ];
+
+        this.setState( newState );
+    }
+
+    render() {
+        let headerClasses = [ Styles.header ];
+        if( this.state.isMenuVisible ){
+            headerClasses.push( Styles.menuVisible );
+            if( this.state.isDestinationVisible || this.state.isAboutVisible ){
+                headerClasses.push( Styles.expanded );
+            }
+        }
+
+        let searchMessageClasses = [ Styles.searchMessage ];
+        if( this.state.isSearchError ){
+            searchMessageClasses.push( Styles.visible );
+        }
+
+        return(
+            <Fragment>
+                <header id="header" className={ headerClasses.join( " " ) }>
+                    <nav className={ Styles.navigation }>
+                        <button className={ Styles.burger } onClick={ this.enableMenuHandler }>
+                            <i className="fa fa-bars" aria-hidden="true"></i>
+                        </button>
+
+                        <a className={ Styles.home } href="/">
+                            <img className={ Styles.logo } src={ require("../../Assets/logo_small.png") } alt="multi-colored petals" />
+                            <h1 className={ Styles.title }>Travel Agency</h1>
+                        </a>
+
+                        <Dropdown selected={ this.state.isDestinationVisible } className={ Styles.dropdown } title="Destinations"
+                            click={ (event) => { this.dropdownHandler( event, "isDestinationVisible" ) } } 
+                            links={[ 
+                                {title: "Africa", href: "#"},
+                                {title: "Europe", href: "#"},
+                                {title: "Americas", href: "#"},
+                                {title: "Asia and Oceania", href: "#"}
+                            ]}>
+                        </Dropdown>
+
+                        <Dropdown selected={ this.state.isAboutVisible } className={ Styles.dropdown } title="About us"
+                            click={ (event) => { this.dropdownHandler( event, "isAboutVisible" ) } } 
+                            links={[ 
+                                {title: "Who we are", href: "#"},
+                                {title: "Contact us", href: "#"},
+                                {title: "Privacy Policy", href: "#"},
+                                {title: "Subscribe to our newsletter!", href: "#", class: "subscribe"}
+                            ]}>
+                        </Dropdown>
+                    </nav>
+
+                    <div class={ Styles.searchBox }>
+                        <input id="search-input" type="text" placeholder="Search" />
+                        <button onClick={ this.searchHandler }>
+                            <span className="fa fa-search"></span>
+                        </button>
+                    </div>
+
+                    
+                
+                </header>
+                
+                <div className={ searchMessageClasses.join( " " ) }>
+                    No destination matches your search
+                </div>
+            
+            </Fragment>
+        );
+    }
 }
 
-export default header;
+export default Header;
